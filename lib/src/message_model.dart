bc67201087cfcdacaba80af0aa98ff292c8ee12e
mashik-lit <m.ashique@lilacinfotech.com>
2022-10-03 15:26:48 +0530

    bug fixes

--- lib/src/message_model.dart
+++ lib/src/message_model.dart
@@ -12,7 +12,7 @@ class MessageModel {
   final String message;
 
   @override
-  String toString() => '{"type": "$typeString", "message": "$message"}';
+  String toString() => '{"type": "${typeString(type)}", "message": "$message"}';
 
   factory MessageModel.textMessage(String message) => MessageModel(
         message: message,
@@ -28,46 +28,6 @@ class MessageModel {
         message: 'join_audio',
         type: MessageTypes.audioCallRequest,
       );
-
-  String get typeString {
-    switch (type) {
-      case MessageTypes.text:
-        return 'text';
-      case MessageTypes.prescription:
-        return 'prescription';
-      case MessageTypes.image:
-        return 'image';
-      case MessageTypes.audio:
-        return 'audio';
-      case MessageTypes.videoCallRequest:
-        return 'video_call_request';
-      case MessageTypes.video:
-        return 'video';
-      case MessageTypes.audioCallRequest:
-        return 'audio_call_request';
-    }
-  }
-
-  static MessageTypes getType(String string) {
-    switch (string) {
-      case 'text':
-        return MessageTypes.text;
-      case 'prescription':
-        return MessageTypes.prescription;
-      case 'image':
-        return MessageTypes.image;
-      case 'audio':
-        return MessageTypes.audio;
-      case 'video_call_request':
-        return MessageTypes.videoCallRequest;
-      case 'video':
-        return MessageTypes.video;
-      case 'audio_call_request':
-        return MessageTypes.audioCallRequest;
-      default:
-        return MessageTypes.text;
-    }
-  }
 
   factory MessageModel.fromMap(Map<String, dynamic> map) {
     return MessageModel(
--- lib/src/message_types.dart
+++ lib/src/message_types.dart
@@ -7,3 +7,42 @@ enum MessageTypes {
   audioCallRequest,
   videoCallRequest
 }
+
+String typeString(MessageTypes type) {
+    switch (type) {
+      case MessageTypes.text:
+        return 'text';
+      case MessageTypes.prescription:
+        return 'prescription';
+      case MessageTypes.image:
+        return 'image';
+      case MessageTypes.audio:
+        return 'audio';
+      case MessageTypes.videoCallRequest:
+        return 'video_call_request';
+      case MessageTypes.video:
+        return 'video';
+      case MessageTypes.audioCallRequest:
+        return 'audio_call_request';
+    }
+  }
+ MessageTypes getType(String string) {
+    switch (string) {
+      case 'text':
+        return MessageTypes.text;
+      case 'prescription':
+        return MessageTypes.prescription;
+      case 'image':
+        return MessageTypes.image;
+      case 'audio':
+        return MessageTypes.audio;
+      case 'video_call_request':
+        return MessageTypes.videoCallRequest;
+      case 'video':
+        return MessageTypes.video;
+      case 'audio_call_request':
+        return MessageTypes.audioCallRequest;
+      default:
+        return MessageTypes.text;
+    }
+  }
